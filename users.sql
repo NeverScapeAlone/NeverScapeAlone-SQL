7d9b34943c20e5dd90cd726e84e2469b4593e152
Ferrariic <thearcticfalcon@gmail.com>
2022-06-29 20:46:35 -0400

    Update users.sql

--- users.sql
+++ users.sql
@@ -2,7 +2,9 @@ CREATE TABLE `users` (
  `user_id` int NOT NULL AUTO_INCREMENT COMMENT 'auto-incremented user ID',
  `login` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'username',
  `discord` varchar(64) DEFAULT NULL,
+ `runewatch` tinytext,
+ `wdr` tinytext,
  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'account creation timestamp',
  PRIMARY KEY (`user_id`),
  UNIQUE KEY `login` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=3733 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Basic information for users of the NeverScapeAlone plugin'
+) ENGINE=InnoDB AUTO_INCREMENT=3743 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Basic information for users of the NeverScapeAlone plugin'
