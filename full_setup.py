4ce09dd35c36573ff8f98b52e1c6c9b1d8596d9e
Ferrariic <thearcticfalcon@gmail.com>
2022-07-03 14:20:32 -0400

    Update full_setup.py

--- full_setup.py
+++ full_setup.py
@@ -1,10 +1,14 @@
 CREATE TABLE `users` (
  `user_id` int NOT NULL AUTO_INCREMENT COMMENT 'auto-incremented user ID',
  `login` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'username',
+ `discord` varchar(64) DEFAULT NULL,
+ `verified` tinyint(1) DEFAULT '0',
+ `runewatch` tinytext,
+ `wdr` tinytext,
  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'account creation timestamp',
  PRIMARY KEY (`user_id`),
  UNIQUE KEY `login` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=3732 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Basic information for users of the NeverScapeAlone plugin';
+) ENGINE=InnoDB AUTO_INCREMENT=77823 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Basic information for users of the NeverScapeAlone plugin'
 
 CREATE TABLE `active_matches` (
  `ID` bigint NOT NULL AUTO_INCREMENT,
