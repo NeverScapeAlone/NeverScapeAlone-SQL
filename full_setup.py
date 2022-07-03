1b1d5813d54dbbe07a3b4f21c3207101e287446f
Ferrariic <thearcticfalcon@gmail.com>
2022-07-03 14:21:00 -0400

    Update full_setup.py

--- full_setup.py
+++ full_setup.py
@@ -77,3 +77,18 @@ CREATE TABLE `user_token` (
  KEY `user_id` (`user_id`),
  CONSTRAINT `user_token_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `world_information` (
+ `ID` int NOT NULL AUTO_INCREMENT,
+ `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ `world_number` int NOT NULL,
+ `activity` text NOT NULL,
+ `p2p` tinyint(1) NOT NULL,
+ `f2p` tinyint(1) NOT NULL,
+ `us` tinyint(1) NOT NULL,
+ `eu_central` tinyint(1) NOT NULL,
+ `eu_west` tinyint(1) NOT NULL,
+ `oceania` tinyint(1) NOT NULL,
+ `player_count` int NOT NULL,
+ PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
